813706fb8a93c5c8af0db3729ea85d65bb792983
MohamedAlii59 <your-email@example.com>
2026-08-30 20:33:12 +0300

    editing appsettings backend cors

--- Code/Portfolio/Portfolio/Program.cs
+++ Code/Portfolio/Portfolio/Program.cs
@@ -71,7 +71,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(frontendUrl, builder.Configuration["Frontend:DeployUrl"])
+        policy.WithOrigins(frontendUrl)
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
